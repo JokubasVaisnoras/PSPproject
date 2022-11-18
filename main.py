8e18e0685b2874cbd1cd8ab2c1fe684e2bad12c1
JokubasVaisnoras <jokkbas@gmail.com>
2022-11-18 16:18:38 +0200

    adding customer and srevice info 👌👌👌

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
+import datetime
 
 app = FastAPI()
 
@@ -36,6 +37,21 @@ class OrderedService(BaseModel):
     service_id: int
     created_timestamp: int
 
+class Discount(BaseModel):
+    id:int
+    service_id:int
+    percentage_off:int
+    exact_price:int
+    created_timestamp:datetime.datetime
+
+class User(BaseModel):
+    id: int
+    password: str
+    email: str
+    full_name: str
+    created_date: datetime.datetime
+    phonenumber: str
+    
 
 @app.get("/organizations/{organization_id}/services/search", tags=["services"], response_model=list[Service])
 async def search_services(name: str) -> list[Service]:
@@ -54,4 +70,31 @@ async def handle_reservation() -> OrderedService:
 
 @app.post("/organizations/{organization_id}/reservations/{reservation_id}/addnote", tags=["services"], response_model=OrderedService)
 async def reservation_add_note(note: str = Body()) -> OrderedService:
+    return None
+
+
+@app.post("/organizations/{organization_id}/services/add", tags=["services"], response_model=Service)
+async def add_service() -> Service:
+    return None
+
+
+@app.delete("/organizations/{organization_id}/services/{service_id}/remove", tags=["services"])
+async def remove_service(organization_id:int,service_id:int) -> None:
+    return "succesful"
+
+
+@app.put("/organizations/{organization_id}/services/{service_id}/discounts", tags=["services"],response_model=Discount)
+async def remove_service(organization_id:int,service_id:int) -> Discount:
+    return None
+
+@app.post("/customers/user/add", tags=["Customer"],response_model=User)
+async def create_account() -> User:
+    return None
+
+@app.get("/customers/{customer_id}/info", tags=["Customer"],response_model=User)
+async def check_information(customer_id:int) -> User:
+    return None
+
+@app.put("/customers/{customer_id}/info", tags=["Customer"],response_model=User)
+async def update_information(customer_id:int) -> User:
     return None
