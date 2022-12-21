00ad52a82812937e9aef9f743b7929ddb6953045
Vilius Puškunalis <47086537+puskunalis@users.noreply.github.com>
2022-12-21 23:13:36 +0200

    Cleanup

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
-from fastapi import APIRouter, FastAPI, Depends, HTTPException
-import customer
-import organization
+from fastapi import APIRouter, FastAPI, HTTPException
 import database
 import menu
 import products
 import payments
 import recipe
 from model import *
-from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 database.Base.metadata.create_all(bind=database.engine)
 
 api_router = APIRouter()
-
-api_router.include_router(customer.router)
-api_router.include_router(organization.router)
-
 
 api_router.include_router(menu.router)
 api_router.include_router(products.router)
@@ -26,7 +18,6 @@ api_router.include_router(payments.router)
 api_router.include_router(recipe.router)
 
 app = FastAPI()
-
 
 app.include_router(api_router)
 
